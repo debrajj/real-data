fa869094dd5d538536c8c701d9e9691b9f2471a4
Debraj Roy <debrajroy@Debrajs-MacBook-Air.local>
2025-11-22 19:21:23 +0530

    Remove unused formatBytes function

--- client/src/App.js
+++ client/src/App.js
@@ -133,13 +133,7 @@ function App() {
     });
   };
 
-  const formatBytes = (bytes) => {
-    if (bytes === 0) return '0 Bytes';
-    const k = 1024;
-    const sizes = ['Bytes', 'KB', 'MB', 'GB'];
-    const i = Math.floor(Math.log(bytes) / Math.log(k));
-    return Math.round(bytes / Math.pow(k, i) * 100) / 100 + ' ' + sizes[i];
-  };
+
 
   return (
     <div className="App">
